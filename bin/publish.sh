0e65f8b133f13034f4c51138b89e2c3263eafaeb
Craig Weber <crgwbr@gmail.com>
2024-08-08 13:44:52 -0400

    ci: add commitizen

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
-
-# Validate tag name
-TAG_NAME="$1"
-if [ -z "$TAG_NAME" ]; then
-    echo "Tag name is missing"
-    exit 1
-fi
-
-# Remove the "r" prefix from the tag name
-PREFIX="v"
-VERSION="${TAG_NAME#"$PREFIX"}"
+set -euxo pipefail
 
 # Check git status
 git fetch --all
@@ -29,25 +18,5 @@ else
     exit 1
 fi
 
-# Set version in packages/thelab-ui-js/package.json
-PKG_FILE="package.json"
-PKG_BK_FILE="${PKG_FILE}.bk"
-jq --arg VERSION "$VERSION" '.version=$VERSION' "$PKG_FILE" > "$PKG_BK_FILE"
-mv "$PKG_BK_FILE" "$PKG_FILE"
-npm i
-git add "$PKG_FILE" "package-lock.json"
-
-# Build
-npm run lint
-npm run build
-
-# Commit
-git commit -m "release: ${TAG_NAME}"
-git tag -a "$TAG_NAME" -m "New Release: $TAG_NAME"
-
-# Push branch and tags. CI will see the new tag and run a job to publish the packages.
-git push origin master
-git push origin "$TAG_NAME"
-
-# Publish to NPM
-npm publish
+# Update version and publish via commitizen
+cz bump "$@"
