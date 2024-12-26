47fe11144b48a96510571b6fa421ec19eef2327c
Craig Weber <crgwbr@gmail.com>
2024-12-26 11:51:13 -0500

    chore(deps): lock file maintenance

--- src/main.ts
+++ src/main.ts
@@ -222,10 +222,6 @@ class AccordCLI {
 
         // Dump change events to stdout
         watcher.on("all", (eventType, filePath) => {
-            if (filePath instanceof Error) {
-                console.error(filePath);
-                return;
-            }
             const relPath = path.relative(rootPath, filePath);
             const msg = JSON.stringify(["remote", eventType, relPath]);
             process.stdout.write(`CMD: ${msg}\n`);
@@ -343,10 +339,6 @@ class AccordCLI {
 
             // React to FS changes
             watcher.on("all", (eventType, filePath) => {
-                if (filePath instanceof Error) {
-                    console.error(filePath);
-                    return;
-                }
                 const relPath = path.relative(config.local.root, filePath);
                 this.syncQueue.queue(config, "local", eventType, relPath);
             });
